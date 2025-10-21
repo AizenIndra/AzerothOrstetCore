6a17ae934b9a751acc24cd2abf35c3dcef77a0ee
AizenIndra <liasihranova@gmail.com>
2025-10-21 13:09:25 +0600

    Добавление выдачи очков арена за победу и поражение на арене

--- src/server/game/Battlegrounds/Arena.cpp
+++ src/server/game/Battlegrounds/Arena.cpp
@@ -28,6 +28,7 @@
 #include "WorldSessionMgr.h"
 #include "WorldStateDefines.h"
 #include "WorldStatePackets.h"
+#include "Translate.h"
 
 void ArenaScore::AppendToPacket(WorldPacket& data)
 {
@@ -355,16 +356,19 @@ void Arena::EndBattleground(TeamId winnerTeamId)
                             if (player->GetQuestStatus(26037) == QUEST_STATUS_INCOMPLETE)
                                 player->KilledMonsterCredit(200001);
                         }
-                        // квест на победу 1на1 - 10 побед
+                        // квест на победу 2на2 - 10 побед
                         if (GetArenaType() == ARENA_TYPE_2v2) {
                             if (player->GetQuestStatus(26038) == QUEST_STATUS_INCOMPLETE)
                                 player->KilledMonsterCredit(200002);
                         }
 
+                        player->RewardArenaPoints(winnerArenaTeam->GetRating(), GetArenaType(), true);
+
                         uint32 RankGetRating = (GetArenaType() == ARENA_TYPE_2v2 ? 3 : GetArenaType() == ARENA_TYPE_5v5 ? 2 : 4)
                                              * sWorld->getIntConfig(CONFIG_RANK_SYSTEM_WIN_RATE_ARENA);
                         player->RewardRankPoints(RankGetRating, Player::PVP_BG);
                         player->RewardRankMoney(GetArenaType(), RankGetRating); 
+                        player->RewardArenaPoints(loserArenaTeam->GetRating(), GetArenaType(), false);
 
                         // Last standing - Rated 5v5 arena & be solely alive player
                         if (GetArenaType() == ARENA_TYPE_5v5 && aliveWinners == 1 && player->IsAlive())
--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2669,6 +2669,8 @@ public:
     void RewardRankMoney(uint8 /*type*/, uint32 /*money*/, bool win = true);
     // инфа при входе в игру скоколько опыта и какой ранг
     void LoadPvPRank();
+    // награда очки арены
+    void RewardArenaPoints(uint32 /*rate*/, int8 /*type*/, bool /*win*/);
 
     // Settings
     [[nodiscard]] PlayerSetting GetPlayerSetting(std::string const& source, uint32 index);
