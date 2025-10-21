ef7746b5750ecb0720280967c4c446713874ba36
AizenIndra <liasihranova@gmail.com>
2025-10-21 13:52:05 +0600

    Предмет получение награды за ивент

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2641,7 +2641,8 @@ public:
         PVP_ITEM,
         PVP_KILL,
         PVE_ACHIV,
-        EXCHANGER_HONOR
+        EXCHANGER_HONOR,
+        EVENT_REWARD,
     };
 
     // опыты для каждого ранга
